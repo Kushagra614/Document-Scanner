f4c21eed4fc29ffc1489d8f2eb0a30503ade992e
Kushagra Vardhan <kushagravardhan619@gmail.com>
2024-08-01 12:35:57 +0530

    Adding Final Doc Points

--- Main.cpp
+++ Main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 using namespace cv;
 
 Mat imgOrg, imgGray,imgBlur,imgCanny,imgThre,imgDil,imgErode;
-vector<Point>intialPoints;
+vector<Point>initialPoints, docPoints;
 
 
 Mat preProcessing(Mat img)
@@ -39,20 +39,21 @@ Mat preProcessing(Mat img)
 		int area = contourArea(contours[i]);
 		cout << area<<endl;
 
-		string objectType;
+		//string objectType;
 
 		if (area > 1000)
 		{
-			float peri = arcLength(contours[i]);
-			approxPolyDP(contours[i], conPoly[i], 0.02 * peri, true);
+			float peri = arcLength(contours[i],true);
+			approxPolyDP(contours[i], conPoly[i],0.02*peri, true);
 
-			if (area > maxArea && conPoly[i].size()==4) {
+			if (area > maxArea && conPoly[i].size()== 4) {
 
 				biggest = { conPoly[i][0],conPoly[i][1],conPoly[i][2],conPoly[i][3] };
 				maxArea = area;
+				drawContours(imgOrg, conPoly, i, Scalar(255, 0, 255), 5);
 				
 			}
-			drawContours(imgOrg, conPoly, i, Scalar(255, 0, 255), 2);
+			
 		}
 	}
 	return biggest;
@@ -62,11 +63,28 @@ Mat preProcessing(Mat img)
 	{
 		for (int i = 0; i < points.size(); i++)
 		{
-			circle(imgOrg, points[i], 30, color, FILLED);
-			putText(imgOrg, to_string(i), points[i], FONT_HERSHEY_PLAIN, 5, color, 5);
+			circle(imgOrg, points[i], 10, color, FILLED);
+			putText(imgOrg, to_string(i), points[i], FONT_HERSHEY_PLAIN, 3, color, 3);
 		}
 	}
 
+
+	vector<Point> reorder(vector<Point>points) {
+		vector<Point>newPoints;
+		vector<int> sumPoints, subPoints;
+			
+		for (int i = 0; i < 4; i++)
+		{
+			sumPoints.push_back(points[i].x + points[i].y);
+			subPoints.push_back(points[i].x - points[i].y);
+		}
+		newPoints.push_back(points[min_element(sumPoints.begin(), sumPoints.end()) - sumPoints.begin()]); // index 0
+		newPoints.push_back(points[max_element(subPoints.begin(), subPoints.end()) - subPoints.begin()]); // index 1
+		newPoints.push_back(points[min_element(subPoints.begin(), subPoints.end()) - subPoints.begin()]); // index 2
+		newPoints.push_back(points[max_element(sumPoints.begin(), sumPoints.end()) - sumPoints.begin()]); // index 3
+
+		return newPoints;
+	}
 void main() {
 	string path = "Resources/paper.jpg";
 	imgOrg = imread(path);
@@ -79,8 +97,11 @@ void main() {
 
 
 	// Get Contours: BIGGEST
-	intialPoints = getContours(imgThre);
-
+	initialPoints = getContours(imgThre);
+	drawPoints(initialPoints, Scalar(0, 0, 255));
+	docPoints = reorder(initialPoints);
+	drawPoints(docPoints, Scalar(0, 255, 0));
+	
 	//warp
 
 
